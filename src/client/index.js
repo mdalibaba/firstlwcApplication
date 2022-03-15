5d82435077c6d0eb286f4f06688d43d749340fb2
mdalibaba <mdalibaba393@gmail.com>
2022-03-15 20:55:53 +0530

    conference-app

--- src/client/index.js
+++ src/client/index.js
@@ -5,3 +5,5 @@ import MyApp from 'my/app';
 const app = createElement('my-app', { is: MyApp });
 // eslint-disable-next-line @lwc/lwc/no-document-query
 document.querySelector('#main').appendChild(app);
+
+
